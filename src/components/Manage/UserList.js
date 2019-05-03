bf4fb6df03801c714c656c0ae7bf5d7e0e991107
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-03 15:01:51 -0700

    add new cohort and cohort to user

--- src/components/Manage/UserList.js
+++ src/components/Manage/UserList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 import User from './User';
 
-const UserList = ({users}) => {
+const UserList = ({users, toggleAddCohortToUserForm}) => {
     let userList = users.map(user => {
-        return <User user={user} />
+        return <User user={user} toggleAddCohortToUserForm={toggleAddCohortToUserForm}/>
     })
     return (
         <div className="userList">
             <h2>Travelers</h2>
+            <div className="row">
+                <div className="col">Email</div>
+                <div className="col">Cohort</div>
+            </div>
             <ul class="list-group" style={{display: 'flex', flexDirection: 'column'}}>
                 {userList}
             </ul>
