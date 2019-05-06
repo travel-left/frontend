c854e6ba4d0e67d49a66cff59e997d24d9bd6c2c
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-06 14:21:29 -0700

    initialize communicate component

---
+++ src/containers/Communicate.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Communicate extends Component {
+
+    state = {
+
+    }
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <h1>I am the Communicate container</h1>
+        )
+    }
+}
+
+export default Communicate
