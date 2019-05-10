4582e70d964d56fa45b86da8d6edb443cc5dc5f7
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-10 09:06:08 -0700

    make notification component functional and destructure props

--- src/components/Communicate/Notification.js
+++ src/components/Communicate/Notification.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Notification extends Component {
+const Notification = ({text}) => {
 
-    constructor(props) {
-        super(props)
-        
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <h4>{this.props.text}</h4>
-                </div>
+    return (
+        <div className="container">
+            <div className="row">
+                <h4>{text}</h4>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Notification
