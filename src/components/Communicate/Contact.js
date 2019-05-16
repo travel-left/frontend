d85aaacf45c965491d30a134f3d2a15fb4ed8865
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-16 14:46:32 -0700

    use props instead of mock data

--- src/components/Communicate/Contact.js
+++ src/components/Communicate/Contact.js
@@ -5,12 +5,12 @@ const Contact = ({name, phone, email, photo}) => {
         <div className="col-4">
             <div class="card" style={{width: '15rem', display: 'flex', flexDirection: 'row', margin: '20px 20px 20px 20px'}}>
                 <div className="">
-                    <img src="https://pbs.twimg.com/profile_images/1114204041431605249/p_TkPVR-_400x400.png" class="card-img-top" alt="..." style={{maxWidth: '100px', borderRadius: '50%'}}></img>
+                    <img src={photo} class="card-img-top" alt="..." style={{maxWidth: '100px', borderRadius: '50%'}}></img>
                 </div>
                 <div className="">
-                    <h5 class="card-title">Jordan Boudreau</h5>
-                    <a href="#" class="card-link">559-393-5872</a> <br/>
-                    <a href="#" class="card-link">jordan@travel-left.com</a>
+                    <h5 class="card-title">{name}</h5>
+                    <a href="#" class="card-link">{phone}</a> <br/>
+                    <a href="#" class="card-link">{email}</a>
                 </div>
             </div>
         </div>
