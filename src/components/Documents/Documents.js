a2ddac2c03538efc43cd59c0539151d5e5d19249
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-24 14:27:23 -0700

    update styling on documents feature

--- src/components/Documents/Documents.js
+++ src/components/Documents/Documents.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import DocumentForm from './DocumentForm'
 import DocumentList from './DocumentList'
 import { apiCall } from '../../services/api'
+import Alert from '../Other/Alert';
+import DashboardHeader from "../Other/DashboardHeader";
 
 class Documents extends Component {
     state = {
@@ -40,12 +42,28 @@ class Documents extends Component {
 
     render() {
         let documents = this.state.documents
-        let showDocumentsList = this.state.showDocumentsList ? <DocumentList documents={documents} /> : null
+        let documentsList = this.state.showDocumentsList ? <DocumentList documents={documents} /> : null
         return (
-            <>
-                <DocumentForm submit={this.handleSubmit} />
-                {showDocumentsList}
-            </>
+            <div>
+                <div className="row">
+                    <div className="col-12">
+                        <Alert />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-8">
+                        <DashboardHeader title='Documents' description='Add all of the important documents to share with your travelers here.'/>
+                        {documentsList}
+                    </div>
+                    <div className="col-4" style={{ backgroundColor: '#FBFBFB', height: '100vh', boxShadow: 'rgb(136, 136, 136) 0px 2px 4px' }}>
+                        <div class="card" style={{border: 'none', backgroundColor: '#FBFBFB'}}>
+                            <div class="card-body" style={{marginTop: '20px'}}>
+                                <DocumentForm submit={this.handleSubmit} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
