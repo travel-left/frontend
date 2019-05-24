8c00401bebd940664e7f8b6dfc40a65302a4602a
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-24 16:52:18 -0700

    update styling on signin form

--- src/components/Sigin/AuthForm.js
+++ src/components/Sigin/AuthForm.js
@@ -35,40 +35,31 @@ class AuthForm extends Component {
     render() {
         const { email, password, error } = this.state
         return (
-            <div className="container">
-                <Slideshow />
-                <div className="row justify-content-center">
-                    <div className="col-10 col-sm-10 col-md-6 col-lg-4 col-xl-3 authform">
-                        <img className="mx-auto d-block" src="./logo.png" alt="" />
-                        <h1 id="landing-header">Sign in to explore and connect with the your cohort.</h1>
-                        {error ? <p style={{ color: 'red' }}>{error.message}</p> : null}
-                        <form className="signin" onSubmit={this.handleSubmit}>
-                            <div className="form-row">
-                                <label htmlFor="email" />
-                                <input type="text" id="email" name="email" className="form-control" placeholder="Email" value={email} onChange={this.handleChange} />
-                                <label htmlFor="password" />
-                                <input type="password" id="password" name="password" className="form-control" placeholder="Password" value={password} onChange={this.handleChange} />
-                                <div className="container-fluid">
-                                    <div className="row">
-                                        <div className="col">
-                                            <a href="/" style={{ color: '#82ccdd' }}>
-                                                {' '}
-                                                <strong>GET THE APP</strong>
-                                            </a>
-                                            <a href="/" style={{ color: '#82ccdd' }}>
-                                                {' '}
-                                                <strong>FORGOT PASSWORD</strong>{' '}
-                                            </a>
-                                        </div>
-                                        <div className="col">
-                                            <button className="btn btn-block btn-lg" type="submit" style={{ backgroundColor: '#3c6382', color: 'white' }}>
-                                                SIGN IN
-                                            </button>
-                                        </div>
-                                    </div>
+            <div className="container-fluid" style={{height: '100vh'}}>
+                <div className="row">
+                    <div className="col-6" style={{display: 'flex', alignContent: 'center', justifyContent: 'center'}}>
+                        <div className="card col-8" style={{marginTop: '5vh', boxShadow: 'rgb(136, 136, 136) 0px 2px 4px', borderRadius: '2%'}} >
+                            {error ? <p style={{ color: 'red' }}>{error.message}</p> : null}
+                            <div className="card-body">
+                                <div className="card-title">
+                                    <h1> Start your Free Trial </h1>
+                                    <p>Create your account by filling the form below</p>
                                 </div>
+                                <form className="signin" onSubmit={this.handleSubmit}>
+                                    <div className="form-row">
+                                        <label htmlFor="first">First Name</label>
+                                        <input className="form-control"type="text" name="first" id="first" placeholder='John'/>
+                                        <label htmlFor="last">Last Name</label>
+                                        <input className="form-control"type="text" name="last" id="last" placeholder='Appleseed'/>
+                                        <label htmlFor="email">Email (will also be your username)</label>
+                                        <input type="text" id="email" name="email" className="form-control" placeholder="Email" value={email} onChange={this.handleChange} />
+                                        <label htmlFor="password">Password</label>
+                                        <input type="password" id="password" name="password" className="form-control" placeholder="Password" value={password} onChange={this.handleChange} />  
+                                    </div>
+                                    <button className="btn btn-lg btn-square dark pull-right" type="submit" >SIGN UP</button>
+                                </form>
                             </div>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
