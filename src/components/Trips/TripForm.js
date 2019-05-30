b858ccddab45bb2ef6f6cf0c432387ab998a848c
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-29 18:31:18 -0700

    clean up tripForm

--- src/components/Trips/TripForm.js
+++ src/components/Trips/TripForm.js
@@ -38,22 +38,29 @@ class TripForm extends Component {
         let { name, image, dateStart, dateEnd, description } = this.state.trip
 
         return (
-            <div class="card" style={{border: 'none', backgroundColor: '#FBFBFB'}}>
-                <h3 class="text-center">Enter your trip details: <span onClick={this.closeForm}>X</span></h3>
-                <form onSubmit={this.handleSubmitEvent} >
-                    <div className="form-row" >
-                        <div className="form-group col-10">
-                            <input name="name" className="form-control col-8" type="text" value={name} onChange={this.handleInputChange} placeholder="Trip Name" />
-                            <input name="image" className="form-control col-8" type="text" value={image} onChange={this.handleInputChange} placeholder="www.linkToYourImage.com" />
-                            <input name="dateStart" className="form-control col-8" type="date" value={dateStart} onChange={this.handleInputChange} placeholder="www.linkToYourImage.com" />
-                            <input name="dateEnd" className="form-control col-8" type="date" value={dateEnd} onChange={this.handleInputChange} placeholder="www.linkToYourImage.com" />
-                            <textarea name="description" className="form-control col-8" type="text" value={description} onChange={this.handleInputChange} placeholder="A description for your trip"></textarea>
+            <div class="card" style={{ border: 'none', backgroundColor: '#FBFBFB' }}>
+                <div class="card-body" style={{ marginTop: '20px' }}>
+                    <form onSubmit={this.handleSubmitEvent} >
+                        <h3 class="text-center">Enter your trip details: <span onClick={this.closeForm}>X</span></h3>
+                        <div className="form-row" >
+                            <div className="form-group col-12">
+                                <label htmlFor="name">Trip name</label>
+                                <input name="name" className="form-control col-8" type="text" value={name} onChange={this.handleInputChange} placeholder="Trip Name" />
+                                <label htmlFor="image">Image link</label>
+                                <input name="image" className="form-control col-8" type="text" value={image} onChange={this.handleInputChange} placeholder="www.linkToYourImage.com" />
+                                <label htmlFor="dateStart">Start date</label>
+                                <input name="dateStart" className="form-control col-8" type="date" value={dateStart} onChange={this.handleInputChange} placeholder="www.linkToYourImage.com" />
+                                <label htmlFor="dateEnd">End date</label>
+                                <input name="dateEnd" className="form-control col-8" type="date" value={dateEnd} onChange={this.handleInputChange} placeholder="www.linkToYourImage.com" />
+                                <label htmlFor="description">Description</label>
+                                <textarea name="description" className="form-control col-8" type="text" value={description} onChange={this.handleInputChange} placeholder="A description for your trip"></textarea>
+                            </div>
                         </div>
-                    </div>
-                    <button type="submit" className="btn btn-lg btn-square dark pull-right" style={{ fontSize: '.9em' }}>
-                        SUBMIT
-                    </button>
-                </form>
+                        <button type="submit" className="btn btn-lg btn-square dark pull-right" style={{ fontSize: '.9em' }}>
+                            SUBMIT
+                        </button>
+                    </form>
+                </div>
             </div>
         )
     }
