b1b5999371ed65f30913aa5c6b5fbd0ac3a76aea
Jordan Boudreau <jordboudreau@gmail.com>
2019-05-29 18:04:03 -0700

    remove redux from communicate and move to components, also update styling of contact and notification forms on sidebar

--- src/components/Communicate/Communicate.js
+++ src/components/Communicate/Communicate.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import NotificationForm from '../components/Communicate/NotificationForm'
-import NotificationList from '../components/Communicate/NotificationList'
-import { apiCall } from '../services/api'
-import ContactList from '../components/Communicate/ContactList'
-import ContactForm from '../components/Communicate/ContactForm'
-import Alert from "../components/Other/Alert";
-import DashboardHeader from '../components/Other/DashboardHeader';
+import NotificationForm from './NotificationForm'
+import NotificationList from './NotificationList'
+import { apiCall } from '../../services/api'
+import ContactList from './ContactList'
+import ContactForm from './ContactForm'
+import Alert from "../Other/Alert";
+import DashboardHeader from '../Other/DashboardHeader';
 
 class Communicate extends Component {
     state = {
         contacts: [],
         showContactList: false,
-        showContactForm: false,
         notifications: [],
         showNotificationsList: false,
-        showNotificationsForm: false
     }
 
     constructor(props) {
@@ -57,33 +54,16 @@ class Communicate extends Component {
         apiCall('post', `/api/contact`, { trip_id: this.props.currentTrip._id, ...contact }).then(() => {
             return this.setState(prevState => {
                 return {
-                    contacts: [...prevState.contacts, contact],
-                    showContactForm: false
+                    contacts: [...prevState.contacts, contact]
                 }
             })
         })
     }
 
-    onNewContactClick = () => {
-        this.setState({
-            showContactForm: true
-        })
-    }
-
     render() {
-        let { showNotificationsList, notifications, showContactForm, showContactList, contacts } = this.state
-        let { currentTrip } = this.props
-        let contactForm,
-            contactList = null
+        let { showNotificationsList, notifications, showContactList, contacts } = this.state
+        let contactList = showContactList ? <ContactList contacts={contacts} /> : null
         let notificationsList = showNotificationsList ? <NotificationList notifications={notifications} /> : null
-
-        if (showContactForm) {
-            contactForm = <ContactForm submit={this.createContact} />
-        }
-
-        if (showContactList) {
-            contactList = <ContactList contacts={contacts} />
-        }
 
         return (
             <div class="">
@@ -107,10 +87,7 @@ class Communicate extends Component {
                     <div className="col-4" style={{ backgroundColor: '#FBFBFB', height: '100vh', boxShadow: 'rgb(136, 136, 136) 0px 2px 4px' }}>
                         <div class="card" style={{ border: 'none', backgroundColor: '#FBFBFB' }}>
                             <div class="card-body" style={{ marginTop: '20px' }}>
-                                <button onClick={this.onNewContactClick} class="btn btn-lg btn-square light" style={{ marginTop: '50px', fontSize: '.9em' }}>
-                                    New Contact
-                                </button>
-                                {contactForm}
+                                <ContactForm submit={this.createContact} />
                                 <NotificationForm submit={this.createNotification} />
                             </div>
                         </div>
@@ -121,13 +98,4 @@ class Communicate extends Component {
     }
 }
 
-const mapStateToProps = state => {
-    return {
-        currentTrip: state.currentTrip
-    }
-}
-
-export default connect(
-    mapStateToProps,
-    null
-)(Communicate)
+export default Communicate
