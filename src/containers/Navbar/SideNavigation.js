4f181f4e7d2a4d8906f8e78715e8fb86c935cd57
Jordan Boudreau <jordboudreau@gmail.com>
2019-06-10 11:51:25 -0700

    remove navbar style sheet

--- src/containers/Navbar/SideNavigation.js
+++ src/containers/Navbar/SideNavigation.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import './Navbar.css'
 
 class SideNavigation extends Component {
     constructor(props) {
