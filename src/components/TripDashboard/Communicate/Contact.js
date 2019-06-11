eaa99ce057b32930f52df4c7fa5a46d1f861da7e
Jordan Boudreau <jordboudreau@gmail.com>
2019-06-10 17:28:15 -0700

    make communicate responsive and use bs classes

--- src/components/TripDashboard/Communicate/Contact.js
+++ src/components/TripDashboard/Communicate/Contact.js
@@ -2,29 +2,19 @@ import React from 'react'
 
 const Contact = ({ name, phone, email, photo }) => {
     return (
-        <div className="card" style={{ minHeight: '80px', boxShadow: 'rgb(136, 136, 136) 0px 0px 8px', marginBottom: '10px' }}>
-            <div className="row no-gutters" style={{ justifyContent: 'space-around', height: '100px', flexDirection: 'row', alignItems: 'center' }}>
-                <div className="col-1">
-                    <img src={photo} className="card-img" alt="..." style={{ maxHeight: '60px', maxWidth: '60px', borderRadius: '50%', marginLeft: '15px' }} />
-                </div>
-                <div className="col-3 hover">
-                    <p className="card-text" style={{ padding: '15px 5px 15px 5px', fontSize: '1em', color: '#3A3A3A', fontWeight: '600', marginLeft: '25px' }}>
-                        {name}
-                    </p>
-                </div>
-                <div className="col-2 hover">
-                    <p className="card-text" style={{ padding: '15px 0px', fontSize: '1em', color: '#3A3A3A', fontWeight: '600' }}>
-                    </p>
+        <div className="card py-2 shadow my-3">
+            <div className="row no-gutters d-flex flex-row justify-content-around align-items-center">
+                <div className="col-1 d-none d-md-flex">
+                    <img src={photo} className="card-img pl-2" alt="..." style={{ maxHeight: '60px', maxWidth: '60px', borderRadius: '50%' }} />
                 </div>
                 <div className="col-3">
-                    <p className="card-text" style={{ padding: '15px 5px 15px 0px', color: '#A3A3A3' }}>
-                        {email}
-                    </p>
+                    {name}
                 </div>
                 <div className="col-3">
-                    <p className="card-text" style={{ padding: '15px 5px 15px 5px' }}>
-                        {phone}
-                    </p>
+                    {email}
+                </div>
+                <div className="col-3">
+                    {phone}
                 </div>
             </div>
         </div>
--- src/components/TripDashboard/Communicate/Notification.js
+++ src/components/TripDashboard/Communicate/Notification.js
@@ -2,19 +2,13 @@ import React from 'react'
 
 const Notification = ({ text }) => {
     return (
-        <div className="card" style={{ minHeight: '80px', boxShadow: 'rgb(136, 136, 136) 0px 0px 8px', marginBottom: '10px' }}>
-            <div className="row no-gutters" style={{ justifyContent: 'space-around', height: '100px', flexDirection: 'row', alignItems: 'center' }}>
-                <div className="col-1">
-                    <img src="" className="card-img" alt="..." style={{ maxHeight: '60px', maxWidth: '60px', borderRadius: '50%', marginLeft: '15px' }} />
-                </div>
-                <div className="col-7 hover">
-                    <p className="card-text" style={{ padding: '15px 5px 15px 5px', fontSize: '1em', color: '#3A3A3A', fontWeight: '600', marginLeft: '25px' }}>
-                    {text}
-                    </p>
-                </div>
+        <div className="card py-2 my-3 shadow">
+            <div className="row no-gutters d-flex flex-row justify-content-around align-items-center">
                 <div className="col-4">
-                    <p className="card-text" style={{ padding: '15px 5px 15px 5px' }}>
-                    </p>
+                    Lorem ipsum dolor sit amet.
+                </div>
+                <div className="col-7">
+                    {text}
                 </div>
             </div>
         </div>
