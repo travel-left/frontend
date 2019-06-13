c2c77a8997ef1ce49774d38cfc872094cbce58d8
Jordan Boudreau <jordboudreau@gmail.com>
2019-06-13 14:05:32 -0700

    move new doucment form from sidebar into modal

--- src/components/TripDashboard/Documents/Documents.js
+++ src/components/TripDashboard/Documents/Documents.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-import DocumentForm from './DocumentForm'
 import DocumentList from './DocumentList'
 import { apiCall } from '../../../util/api'
 import Alert from '../../Other/Alert'
 import DashboardHeader from '../../Other/DashboardHeader'
 import SideBar from '../SideBar'
+import AddDocument from './AddDocument'
 
 class Documents extends Component {
     state = {
@@ -59,9 +59,10 @@ class Documents extends Component {
                 <div className="row">
                     <div className="col-md-8">
                         <DashboardHeader title="Documents" description="Add all of the important documents to share with your travelers here." currentTrip={this.props.currentTrip} />
+                        <AddDocument submit={this.handleSubmit} />
                         {documentsList}
                     </div>
-                    <SideBar ctr={[<DocumentForm submit={this.handleSubmit} />]} />
+                    <SideBar ctr={[]} />
                 </div>
             </div>
         )
