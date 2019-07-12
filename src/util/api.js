545f3bcb56a83a7a087652532c421116107fca38
Jordan Boudreau <jordboudreau@gmail.com>
2019-07-12 01:12:33 -0700

    update api for prod

--- src/util/api.js
+++ src/util/api.js
@@ -10,8 +10,8 @@ export const setTokenHeader = token => {
 
 export const apiCall = (method, path, data) => {
     return new Promise((resolve, reject) => {
-        // return axios[method.toLowerCase()](`https://left-backend.herokuapp.com${path}`, data)
-        return axios[method.toLowerCase()](`http://localhost:8081${path}`, data)
+        return axios[method.toLowerCase()](`https://left-backend.herokuapp.com${path}`, data)
+            // return axios[method.toLowerCase()](`http://localhost:8081${path}`, data)
             .then(res => {
                 return resolve(res.data)
             })
