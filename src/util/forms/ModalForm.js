ffc07c0418c4214df7ed8330b2e5d435e2cef26f
Jordan Boudreau <jordboudreau@gmail.com>
2019-07-18 16:31:56 -0700

    animate modalForm

--- src/util/forms/ModalForm.js
+++ src/util/forms/ModalForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Formik, Form } from 'formik'
-import { Portal } from 'react-portal'
-
+import Mortal from 'react-mortal'
 // EXPECTED PROPS:
 //     title: this is the title of the modal
 //     validationSchema: this is a yup validation validationSchema
@@ -18,87 +17,86 @@ import { Portal } from 'react-portal'
 class ModalForm extends Component {
 
     state = {
-        open: false,
-        transition: false
+        open: false
     }
 
     handleRemove = () => {
         this.props.remove()
-        this.closeModal()
+        this.toggleModal()
     }
 
-    openModal = () => {
+    toggleModal = () => {
         this.setState(prevState => ({ open: !prevState.open }))
-        document.getElementById('app-root').classList.remove('darkFadeOut')
-        document.getElementById('app-root').classList.add('darkFadeIn')
-    }
-
-    closeModal = () => {
-        this.setState(prevState => ({ transition: !prevState.transition }))
-        document.getElementById('app-root').classList.remove('darkFadeIn')
-        document.getElementById('app-root').classList.add('darkFadeOut')
-        setTimeout(() => {
-            this.setState(prevState => ({ open: !prevState.open, transition: !prevState.transition }))
-        }, 500);
     }
 
     render() {
-        let modalClass = this.state.open ? 'show d-block animated fadeIn' : ''
-        let fadeOut = this.state.transition ? 'show d-block animated fadeOut' : ''
         let { title, validationSchema, initialValues, submit, remove, icon, button } = this.props
 
-        let opener = icon ? <i className={`hover ${icon}`} onClick={this.openModal} ></i> :
-            <button className={`btn ${button.classes}`} onClick={this.openModal} >{button.text}</button>
+        let opener = icon ? <i className={`hover ${icon}`} onClick={this.toggleModal} ></i> :
+            <button className={`btn ${button.classes}`} onClick={this.toggleModal} >{button.text}</button>
 
-        let modal = !this.state.open ? null :
-            (
-                <Portal>
-                    <Formik
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={(values, actions) => {
-                            submit(values)
-                            actions.setSubmitting(false)
-                            actions.resetForm()
-                            this.closeModal()
-                        }}
-                        onReset={(values, actions) => {
-                            actions.resetForm()
-                            this.closeModal()
-                        }}
-                    >
-                        {(actions) => (
-                            <div className={`modal fade ${modalClass} ${fadeOut}`} style={{ filter: 'none' }}>
+        return (
+            <>
+                {opener}
+                <Mortal
+                    isOpened={this.state.open}
+                    onClose={this.toggleModal}
+                    motionStyle={(spring, isVisible) => ({
+                        opacity: spring(isVisible ? 1 : 0),
+                        modalOffset: spring(isVisible ? 0 : -90, {
+                            stiffness: isVisible ? 300 : 200,
+                            damping: isVisible ? 15 : 30,
+                        }),
+                    })}
+                >
+                    {(motion, isVisible) => (
+                        <Formik
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={(values, actions) => {
+                                submit(values)
+                                actions.setSubmitting(false)
+                                actions.resetForm()
+                                this.toggleModal()
+                            }}
+                            onReset={(values, actions) => {
+                                actions.resetForm()
+                                this.toggleModal()
+                            }}
+                        >
+                            <div className='modal d-block'>
+                                <div
+                                    className="Modal--overlay"
+                                    style={{
+                                        opacity: motion.opacity,
+                                        pointerEvents: isVisible ? 'auto' : 'none',
+                                    }}
+                                    onClick={this.toggleModal}
+                                />
                                 <div className="modal-dialog" role="document">
-                                    <div className="modal-content">
+                                    <div className="modal-content" style={{
+                                        opacity: motion.opacity,
+                                        transform: `translate3d(0, ${motion.modalOffset}px, 0)`,
+                                    }}>
                                         <Form>
                                             <div className="modal-header">
-                                                <h5 className="modal-title" id="addnewNameModal">
-                                                    {title}
-                                                </h5>
+                                                <h5 className="modal-title" id="addnewNameModal">{title}</h5>
                                                 <button type="reset" className="close" aria-label="Close">
                                                     <span aria-hidden="true">&times;</span>
                                                 </button>
                                             </div>
-                                            <div className="modal-body">
-                                                {this.props.children}
-                                            </div>
+                                            <div className="modal-body"> {this.props.children}</div>
                                             <hr className="mt-2" />
                                             {remove && <a className="btn btn-lg btn-danger ml-4 mb-4 text-light hover" onClick={this.handleRemove}>DELETE</a>}
                                             <button type="submit" className="btn btn-lg btn-primary float-right mr-4 mb-4">SUBMIT</button>
                                         </Form>
                                     </div>
+
                                 </div>
                             </div>
-                        )}
-                    </Formik>
-
-                </Portal>
-            )
-        return (
-            <>
-                {opener}
-                {modal}
+                        </Formik>
+                    )}
+                </Mortal>
             </>
         )
     }
