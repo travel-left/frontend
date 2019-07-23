930cce3720bc91498143bc1a62650743bb1ccab8
Jordan Boudreau <jordboudreau@gmail.com>
2019-07-23 14:17:43 -0700

    update travelers

--- src/TripDashboard/Travelers/Travelers/UpdateTravelerForm.js
+++ src/TripDashboard/Travelers/Travelers/UpdateTravelerForm.js
@@ -15,7 +15,7 @@ export default function UpdateTravelerForm(props) {
             .min(2, 'Please enter a longer name')
             .max(50, 'Please enter a shorter name')
             .required('Please enter a name'),
-        img: Yup.string().required('Please upload an image'),
+        image: Yup.string().required('Please upload an image'),
         email: Yup.string().email('please enter a valid email')
     })
 
@@ -25,7 +25,7 @@ export default function UpdateTravelerForm(props) {
         <ModalForm icon={icon} header="Edit traveler" validationSchema={schema} initialValues={initialValues} {...props}>
             <FormField name="name" label="Name" placeholder="Steve Jobs" />
             <SelectField name="status" options={[{ name: 'INVITED', value: 'INVITED' }, { name: 'CONFIRMED', value: 'CONFIRMED' }, { name: 'ON-TRIP', value: 'ON-TRIP' }, { name: 'POST-TRIP', value: 'POST-TRIP' }]} label='Status' />
-            <FormField name="img" label="Upload a new image" component={Uploader} />
+            <FormField name="image" label="Upload a new image" component={Uploader} />
             <FormField name="email" label="Email" placeholder="steve@apple.com" type="email" />
             <FormField name="phone" label="Phone number" placeholder="559-867-5309" type="text" />
             <FormField name="personalNotes" label="Personal notes" component="textarea" placeholder="Any extra notes about this traveler" className="d-block" />
