75fcf6ffe62be877b0a3dd6c3422016fc7262cfb
Jordan Boudreau <jordboudreau@gmail.com>
2019-07-24 11:39:33 -0700

    move duplicate code into helper for event

--- src/TripDashboard/Itinerary/Event/UpdateEventForm.js
+++ src/TripDashboard/Itinerary/Event/UpdateEventForm.js
@@ -3,9 +3,8 @@ import moment from 'moment-timezone'
 import FormField from '../../../util/forms/FormField'
 import SelectField from '../../../util/forms/SelectField'
 import Uploader from '../../../util/forms/Uploader'
-import * as Yup from 'yup'
 import ModalForm from '../../../util/forms/ModalForm'
-import { nameValidator, dateValidator, fileValidator, descriptionValidator } from '../../../util/validators'
+import { schema, types, timezones } from "./EventHelpers"
 
 export default function UpdateEventForm(props) {
     const { event } = props
@@ -24,62 +23,6 @@ export default function UpdateEventForm(props) {
         dateEnd: event.dateEnd,
         timeEnd: event.dtEnd.split(' ')[0]
     }
-
-    let timeZones = moment.tz.names().map(name => {
-        const offset = moment.tz(name).format('Z')
-        const abbrev = moment.tz(name).format('z')
-        return {
-            name: `(UTC${offset}) ${name.replace('_', ' ')} (${abbrev})`,
-            value: name,
-            offset: offset
-        }
-    })
-
-    timeZones = timeZones.sort((f, s) => {
-        return parseInt(f.offset, 10) - parseInt(s.offset, 10)
-    })
-
-    const types = [
-        {
-            name: 'Category',
-            value: '',
-            hidden: true,
-            default: true
-        },
-        {
-            name: 'Lodging',
-            value: 'lodging'
-        },
-        {
-            name: 'Event',
-            value: 'event'
-        },
-        {
-            name: 'Transportation',
-            value: 'transportation'
-        },
-        {
-            name: 'Flight',
-            value: 'flight'
-        }
-    ]
-
-    const schema = Yup.object().shape({
-        name: nameValidator,
-        tzStart: Yup.string('Time zone must be a string'),
-        tzEnd: Yup.string('Time zone must be a string'),
-        category: Yup.string('Category must be a string'),
-        description: descriptionValidator,
-        image: fileValidator,
-        link: Yup.string('Link must be a string'),
-        linkDescription: Yup.string('Link text must be a string'),
-        dateStart: dateValidator,
-        timeStart: Yup.string('Time is not valid'),
-        dateEnd: dateValidator,
-        timeEnd: Yup.string('Time is not valid')
-    })
-
-
 
     return (
         <ModalForm icon='hover far fa-edit fa-2x text-secondary' header="Edit your event" validationSchema={schema} initialValues={initialValues} submit={props.submit} remove={props.remove}>
@@ -109,10 +52,10 @@ export default function UpdateEventForm(props) {
             </div>
             <div className="form-row">
                 <div className="col-6">
-                    <SelectField name="tzStart" options={timeZones} />
+                    <SelectField name="tzStart" options={timezones} />
                 </div>
                 <div className="col-6">
-                    <SelectField name="tzEnd" options={timeZones} />
+                    <SelectField name="tzEnd" options={timezones} />
                 </div>
             </div>
             <FormField component="textarea" name="description" cols="70" rows="2" placeholder="A summary of your event" label='Event summary' />
