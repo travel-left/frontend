e7ac7793dd3f243803f1be38e1ae6e7206b8a3d8
Jordan Boudreau <jordboudreau@gmail.com>
2019-07-30 14:42:12 -0700

    animate left card

--- src/util/LeftCard.js
+++ src/util/LeftCard.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {useSpring, animated} from 'react-spring'
 
 export default (props) => (
-    <div className="col-md-4 my-2">
+    <animated.div className="col-md-4 my-2" style={useSpring({opacity: 1, from: {opacity: 0}})}>
         <div className="card shadow mx-2">
             <div className="p-3">
                 {props.children}
             </div>
         </div>
-    </div>)
+    </animated.div>)
