d67e9ed74251b162bc634ca7df6d28c2bd4b895b
Jordan Boudreau <jordboudreau@gmail.com>
2019-07-31 13:35:23 -0700

    fix crash on selecting travelers

--- src/TripDashboard/Travelers/index.js
+++ src/TripDashboard/Travelers/index.js
@@ -169,7 +169,7 @@ class Travelers extends Component {
             travelerStatuses
         )
         const allTravelers = [...newTravelers, ...updatedTravelers]
-        this.filterTravelers(null, allTravelers)
+        this.filterTravelers(this.state.filters, allTravelers)
     }
 
     textSelectedTravelers = text => {
