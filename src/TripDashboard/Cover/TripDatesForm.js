9b45c51964cefea37a4beb0f607f1c03f529bb23
Cameron Podd <podd@usc.edu>
2019-07-31 10:48:26 -0700

    Get rid of moment timezone bug

--- src/TripDashboard/Cover/TripDatesForm.js
+++ src/TripDashboard/Cover/TripDatesForm.js
@@ -16,18 +16,39 @@ export default function TripDatesForm({ dateStart, dateEnd, submit }) {
         dateEnd: dateValidator
     })
 
+    const dateStartMoment = dateStart.split('T')[0]
+    const dateEndMoment = dateEnd.split('T')[0]
+
     const button = {
         classes: 'text-light hover',
         text: (
             <h5>
-                <Moment date={dateStart} format="MMMM DD" /> {' - '} <Moment date={dateEnd} format="MMMM DD" /> <i className="far fa-calendar-alt ml-1" />
+                <Moment date={dateStartMoment} format="MMMM DD" /> {' - '}{' '}
+                <Moment date={dateEndMoment} format="MMMM DD" />{' '}
+                <i className="far fa-calendar-alt ml-1" />
             </h5>
         )
     }
     return (
-        <ModalForm button={button} header="Trip dates" validationSchema={schema} initialValues={initialValues} submit={submit}>
-            <FormField name="dateStart" label="Trip Start Date" placeholder={initialValues.dateStart} type="date" />
-            <FormField name="dateEnd" label="Trip End Date" placeholder={initialValues.dateEnd} type="date" />
+        <ModalForm
+            button={button}
+            header="Trip dates"
+            validationSchema={schema}
+            initialValues={initialValues}
+            submit={submit}
+        >
+            <FormField
+                name="dateStart"
+                label="Trip Start Date"
+                placeholder={initialValues.dateStart}
+                type="date"
+            />
+            <FormField
+                name="dateEnd"
+                label="Trip End Date"
+                placeholder={initialValues.dateEnd}
+                type="date"
+            />
         </ModalForm>
     )
 }
