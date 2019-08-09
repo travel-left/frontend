f93b31bf7c44131c033baa31bb4d8b24246ca6e8
Jordan Boudreau <jordboudreau@gmail.com>
2019-08-08 22:22:35 -0700

    add hover to checkbox

--- src/util/forms/Checkbox.js
+++ src/util/forms/Checkbox.js
@@ -2,19 +2,19 @@ import React from 'react'
 
 export default ({ name = '', label = '', className, checked, onChange }) => (
     <div
-        className={`custom-control custom-checkbox ${className}`}
+        className={`custom-control custom-checkbox ${className} hover`}
         // onClick={onChange}
         onClick={label === 'noshow' ? onChange : null}
     >
         <input
             type="checkbox"
-            className="custom-control-input"
+            className="custom-control-input hover"
             checked={checked}
             name={name}
             onChange={onChange}
             id={name}
         />
-        <label htmlFor={name} className="custom-control-label">
+        <label htmlFor={name} className="custom-control-label hover">
             {label && label !== 'noshow' ? label : null}
         </label>
     </div>
