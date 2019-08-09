41bc7a8e21d36146588de61bed1fcd31b8549c16
Jordan Boudreau <jordboudreau@gmail.com>
2019-08-09 12:24:45 -0700

    sort events by date on share

--- src/TripDashboard/Share/index.js
+++ src/TripDashboard/Share/index.js
@@ -35,6 +35,7 @@ class Share extends Component {
         events.forEach(event => {
             if (!days.includes(event.dateStart)) days.push(event.dateStart)
         })
+        days.sort(date_sort_asc)
         this.setState({ trip: data.trip, events, days, selectedDay: days[0] })
     }
 
@@ -180,6 +181,12 @@ const time_sort_asc = function(event1, event2) {
         moment(event2.dtStart, ['h:mm A']).format('HH:mm')
     )
         return -1
+    return 0
+}
+
+const date_sort_asc = function(date1, date2) {
+    if (date1 > date2) return 1
+    if (date1 < date2) return -1
     return 0
 }
 
