ef1a0cc041c014659ae67c8018d02a96bcb9c407
Jordan Boudreau <jordboudreau@gmail.com>
2019-08-08 22:33:42 -0700

    update styling on change password page

--- src/Auth/NewPassword.js
+++ src/Auth/NewPassword.js
@@ -37,46 +37,50 @@ export default class NewPassword extends Component {
         }
 
         return (
-            <div className="card col-10 shadow align-self-start my-5">
-                {this.error ? (
-                    <p style={{ color: 'red' }}>{this.error.message}</p>
-                ) : null}
-                <div className="card-body p-4">
-                    <h1 className="heading">Change Password</h1>
-                    <h5 className="text-dark">
-                        Change the password of your account.
-                    </h5>
-                    <Formik
-                        initialValues={initialValues}
-                        onSubmit={async (values, actions) => {
-                            await this.submit(values)
-                            actions.setSubmitting(false)
-                        }}
-                        render={({ isSubmitting }) => (
-                            <Form>
-                                <FormField
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    placeholder="••••••••••••"
-                                />
-                                <FormField
-                                    name="confirmPassword"
-                                    label="Confirm Password"
-                                    type="password"
-                                    placeholder="••••••••••••"
-                                />
-                                <button
-                                    className="btn btn-lg btn-primary float-right"
-                                    style={{ marginTop: '35px' }}
-                                    type="submit"
-                                    disabled={isSubmitting}
-                                >
-                                    Change Password
-                                </button>
-                            </Form>
-                        )}
-                    />{' '}
+            <div className="container d-flex justify-content-center align-items-center">
+                <div className="col-8 d-flex mt-5 d-flex justify-content-center align-items-center">
+                    <div className="card shadow">
+                        {this.error ? (
+                            <p style={{ color: 'red' }}>{this.error.message}</p>
+                        ) : null}
+                        <div className="card-body p-4">
+                            <h1 className="heading">Change Password</h1>
+                            <h5 className="text-dark">
+                                Change the password of your account.
+                            </h5>
+                            <Formik
+                                initialValues={initialValues}
+                                onSubmit={async (values, actions) => {
+                                    await this.submit(values)
+                                    actions.setSubmitting(false)
+                                }}
+                                render={({ isSubmitting }) => (
+                                    <Form>
+                                        <FormField
+                                            name="password"
+                                            label="Password"
+                                            type="password"
+                                            placeholder="••••••••••••"
+                                        />
+                                        <FormField
+                                            name="confirmPassword"
+                                            label="Confirm Password"
+                                            type="password"
+                                            placeholder="••••••••••••"
+                                        />
+                                        <button
+                                            className="btn btn-lg btn-primary float-right"
+                                            style={{ marginTop: '35px' }}
+                                            type="submit"
+                                            disabled={isSubmitting}
+                                        >
+                                            Change Password
+                                        </button>
+                                    </Form>
+                                )}
+                            />{' '}
+                        </div>
+                    </div>
                 </div>
             </div>
         )
