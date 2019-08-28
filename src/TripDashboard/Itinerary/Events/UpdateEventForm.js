4c3dce331c659b861ee8fa81cf6104cf5efeea7d
Jordan Boudreau <jordboudreau@gmail.com>
2019-08-28 14:41:24 -0700

    bug fixes with time

--- src/TripDashboard/Itinerary/Events/UpdateEventForm.js
+++ src/TripDashboard/Itinerary/Events/UpdateEventForm.js
@@ -5,6 +5,7 @@ import Uploader from '../../../util/forms/Uploader'
 import ModalForm from '../../../util/forms/ModalForm'
 import { schema, types, timezones } from './EventHelpers'
 import { FieldArray } from 'formik'
+import moment from 'moment'
 
 export default function UpdateEventForm(props) {
     const { event } = props
@@ -18,9 +19,9 @@ export default function UpdateEventForm(props) {
         documents: event.documents,
         address: event.address,
         dateStart: event.dateStart,
-        timeStart: event.dtStart.split(' ')[0],
+        timeStart: moment(event.dtStart, ["h:mm A"]).format("HH:mm"),
         dateEnd: event.dateEnd,
-        timeEnd: event.dtEnd.split(' ')[0]
+        timeEnd: moment(event.dtEnd, ["h:mm A"]).format("HH:mm"),
     }
 
     return (
