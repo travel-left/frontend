bb86b965215c79d3fe9745f26566eed24da4d4fd
Jordan Boudreau <jordboudreau@gmail.com>
2019-10-23 16:55:50 -0700

    add all fields for registering account

--- src/OrganizationDashboard/RegisterAccountModalForm.js
+++ src/OrganizationDashboard/RegisterAccountModalForm.js
@@ -25,11 +25,18 @@ export default class RegisterAccountModalForm extends Component {
         address: '',
         tax_id: '',
         url: '',
-        mcc: 4722
-    }
-
-    handleOnChange = e => {
-        // this.setState({ [e.target.name]: e.target.value })
+        mcc: 4722,
+        firstName: '',
+        lastName: '',
+        dob: '',
+        repAddress: '',
+        ssn: '',
+        title: '',
+        email: '',
+        repPhone: '',
+        ownerFirstName: '',
+        ownerLastName: '',
+        ownerEmail: ''
     }
 
     next = e => {
@@ -42,14 +49,8 @@ export default class RegisterAccountModalForm extends Component {
     }
 
     handleSubmit = e => {
-        const { name, email, phone, personalNotes } = this.state
         e.preventDefault()
-        this.props.submit({
-            name,
-            email,
-            phone,
-            personalNotes
-        })
+        console.log(this.state)
         this.handleToggleModal()
     }
 
@@ -78,7 +79,7 @@ export default class RegisterAccountModalForm extends Component {
 
     render() {
         const steps = ['Company Information', 'Representative Information', 'Owner Information']
-        let { step } = this.state
+        let { step, firstName, lastName, dob, repAddress, ssn, title, email, repPhone, ownerEmail, ownerLastName, ownerFirstName } = this.state
 
         return (
             <Portal >
@@ -104,7 +105,33 @@ export default class RegisterAccountModalForm extends Component {
                                         </Step>
                                     ))}
                                 </Stepper>
-                                <CompanySection handleChange={this.handleCompanyChange} name={this.state.name} number={this.state.number} address={this.state.address} tax_id={this.state.tax_id} url={this.state.url}></CompanySection>
+
+                                {step === 0 && <CompanySection
+                                    handleChange={this.handleCompanyChange}
+                                    name={this.state.name}
+                                    number={this.state.number}
+                                    address={this.state.address}
+                                    tax_id={this.state.tax_id}
+                                    url={this.state.url}
+                                ></CompanySection>}
+
+                                {step === 1 && <RepresentativeSection
+                                    handleChange={this.handleCompanyChange}
+                                    firstName={firstName}
+                                    lastName={lastName}
+                                    dob={dob}
+                                    repAddress={repAddress}
+                                    ssn={ssn}
+                                    title={title}
+                                    email={email}
+                                    repPhone={repPhone}
+                                ></RepresentativeSection>}
+                                {step === 2 && <OwnerSection
+                                    handleChange={this.handleCompanyChange}
+                                    ownerFirstName={ownerFirstName}
+                                    ownerLastName={ownerLastName}
+                                    ownerEmail={ownerEmail}
+                                ></OwnerSection>}
                                 <hr className="my-4" />
                                 {/* <Button type="submit" className="float-right" size="large" variant="contained" color="primary" style={{ width: '180px', height: '50px' }} onClick={this.handleSubmit}>SUBMIT</Button> */}
                                 <StepThroughButtons step={this.state.step} length={2} back={this.back} next={this.next} onSubmit={this.handleSubmit}></StepThroughButtons>
@@ -213,6 +240,204 @@ const CompanySection = ({ handleChange, url, name, number, address, tax_id }) =>
     )
 }
 
+const RepresentativeSection = ({ handleChange, firstName, lastName, dob, repAddress, ssn, title, email, repPhone }) => {
+    // --------- rep first name
+    // --------- rep last name
+    // --------- rep DOB
+    // --------- rep address
+    // --------- last four SSN
+    // --------- rep relationship title
+    // --------- rep email
+    // --------- rep phone
+    return (
+        <>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="first name"
+                        value={firstName}
+                        placeholder="Your first name"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="firstName"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="last name"
+                        value={lastName}
+                        placeholder="Your last name"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="lastName"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Date of birth"
+                        value={dob}
+                        placeholder="Your birthday"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="dob"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="addresss"
+                        value={repAddress}
+                        placeholder="Your address"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="repAddress"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Last four digits of SSN"
+                        value={ssn}
+                        placeholder="Last four digits of your SSN"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="ssn"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Title"
+                        value={title}
+                        placeholder="Your title"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="title"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Email"
+                        value={email}
+                        placeholder="Your email"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="email"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Phone number"
+                        value={repPhone}
+                        placeholder="Your phone number"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="repPhone"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
+const OwnerSection = ({ handleChange, ownerFirstName, ownerLastName, ownerEmail }) => {
+    return (
+        <>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="First name"
+                        value={ownerFirstName}
+                        placeholder="Company owner's first name"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="ownerFirstName"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Last name"
+                        value={ownerLastName}
+                        placeholder="Company owner's last name"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="ownerLastName"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-10">
+                    <TextField
+                        required
+                        id="standard-required"
+                        label="Email"
+                        value={ownerEmail}
+                        placeholder="Company owner's email"
+                        margin="normal"
+                        className="mx-2 mt-4"
+                        onChange={handleChange}
+                        name="ownerEmail"
+                        style={{ width: '100%' }}
+                    />
+                </div>
+            </div>
+        </>
+    )
+}
+
 const StepThroughButtons = ({ length, step, back, next, onSubmit, onDestroy }) => {
     let buttons
     if (step === 0) {//first page
@@ -227,7 +452,7 @@ const StepThroughButtons = ({ length, step, back, next, onSubmit, onDestroy }) =
         buttons = (
             <div className="float-right">
                 <button className="btn btn-lg btn-link text-primary" onClick={back}>PREV</button>
-                <Button size="large" variant="contained" color="primary" style={{ width: '180px', height: '50px', color: "white" }} onClick={next}>
+                <Button size="large" variant="contained" color="primary" style={{ width: '180px', height: '50px', color: "white" }} onClick={onSubmit}>
                     SUBMIT
                 </Button>
             </div>
