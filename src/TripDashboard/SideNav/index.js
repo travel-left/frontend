af154a1503f89d57e679499c35f794fb4015ab15
Jordan Boudreau <jordboudreau@gmail.com>
2019-11-22 16:19:21 -0800

    migrate side nav links to material design

--- src/TripDashboard/SideNav/index.js
+++ src/TripDashboard/SideNav/index.js
@@ -1,44 +1,44 @@
 import React from 'react'
+import List from '@material-ui/core/List'
 import SideNavLink from './SideNavLink'
+import Card from '@material-ui/core/Card'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import Typography from '@material-ui/core/Typography'
+import { withRouter } from 'react-router-dom'
 
-export default function SideNav({ ctId }) {
+export default withRouter(function SideNav({ ctId, location }) {
     const sideNavComponents = [
         {
             name: 'edit',
-            text: 'Trip Information'
+            text: 'Trip Information',
+            active: location.pathname.includes('edit')
         },
         {
             name: 'itinerary',
-            text: 'Manage Itinerary'
+            text: 'Manage Itinerary',
+            divider: true,
+            active: location.pathname.includes('itinerary')
         },
         {
             name: 'travelers',
-            text: 'Manage Travelers'
+            text: 'Manage Travelers',
+            divider: true,
+            active: location.pathname.includes('travelers')
         }
     ]
-    const sideNavList = sideNavComponents.map(({ name, text }) => (
-        <SideNavLink key={name} text={text} name={name} tripId={ctId} />
+    const sideNavList = sideNavComponents.map(({ name, text, divider, active }) => (
+        <SideNavLink key={name} text={text} name={name} tripId={ctId} divider={divider} active={active} />
     ))
     return (
-        <div className="px-0" style={{
-            background: '#FFFFFF',
-            boxShadow: '-2px 2px 4px 0 rgba(0, 0, 0, 0.3)',
-            borderRadius: '8px'
-        }}>
-            <ul className="list-group list-group-flush">
-                {sideNavList}
-                <a href={`/trips/${ctId}/preview`} target="_blank" className="list-group-item d-flex justify-content-between align-items-center border-right-0 border-left-0 py-3"
-                    activeClassName="active"
-                    style={{
-                        fontFamily: 'Roboto',
-                        fontSize: '14px',
-                        fontWeight: '500',
-                        color: '#666666',
-                        borderRadius: '3px'
-                    }}
-                >
-                    Trip Preview</a>
-            </ul>
-        </div>
+        <List component="div" style={{ paddingTop: 0, paddingBottom: 0 }}>
+            {sideNavList}
+            <Card>
+                <Divider />
+                <ListItem onClick={() => window.open(`/trips/${ctId}/preview`)} button className='d-flex justify-content-between align-items-center' style={{ padding: '12px 16px' }}>
+                    <Typography variant="h6" >Trip Preview</Typography>
+                </ListItem>
+            </Card>
+        </List>
     )
-}
+})
--- src/util/otherComponents/SideNavItem.js
+++ src/util/otherComponents/SideNavItem.js
@@ -3,7 +3,6 @@ import Typography from '@material-ui/core/Typography'
 import Chip from '@material-ui/core/Chip'
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
 import Card from '@material-ui/core/Card';
 
 export default ({ text, total, active, handleClick, divider }) => {
@@ -11,16 +10,9 @@ export default ({ text, total, active, handleClick, divider }) => {
         <Card>
             {divider && <Divider />}
             <ListItem button className='d-flex justify-content-between align-items-center' onClick={() => handleClick(text)} name={text} style={{ background: active && '#0A58CE', padding: '12px 16px' }}>
-                {/* <ListItemText primary="Trash" /> */}
                 <Typography variant="h6" style={{ color: active ? 'white' : '#666666' }}>{text}</Typography>
-                <Chip color="primary" size="small" label={total} style={{ fontSize: 12, fontWeight: 600, }} />
+                {(total == 0 || total) && <Chip color="primary" size="small" label={total} style={{ fontSize: 12, fontWeight: 600, }} />}
             </ListItem>
         </Card>
     )
 }
-
-{/* <ListItem className='d-flex justify-content-between align-items-center' onClick={handleClick} name={text} style={{ background: active ? '#0A58CE' : 'white', padding: '8px 16px' }}>
-    <Divider />
-    <Typography variant="h6" style={{ color: active ? 'white' : '#666666' }}>{text}</Typography>
-    <Chip color="primary" size="small" label={total} style={{ fontSize: 12, fontWeight: 600, }} />
-</ListItem> */}
