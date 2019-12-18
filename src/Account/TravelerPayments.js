c55c6fdbd73fabf9b9ffc096310fd8cbb1894b56
Jordan Boudreau <jordboudreau@gmail.com>
2019-12-18 14:52:12 -0800

    use reight var

--- src/Account/TravelerPayments.js
+++ src/Account/TravelerPayments.js
@@ -65,7 +65,7 @@ export default class TravelerPayments extends Component {
                     'Your account is not yet verified, please verify your account to request payments from travelers. After you complete the form it may take up to 15 minutes to verify your account.'
                     : 'You have verified your account. You can now request payments from you travelers. Go to your Payment Dashboard by clicking the button below. You can modify your payment settings and view your payouts there.'}</Typography>
                 {!this.state.canRequestPayments && <Button size="large" type="submit" variant="contained" color="secondary" id="signup" style={{ width: '180px', height: '50px', color: 'white', marginTop: '25px' }} onClick={() => {
-                    var win = window.open(`https://connect.stripe.com/express/oauth/authorize?redirect_uri=http://localhost:3000/account/travelerPayments/verify_connect_account&${process.env.STRIPE_CONNECT_CLIENT_ID}&state=${uuid()}`, '_blank');
+                    var win = window.open(`https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://app.travel-left.com/account/travelerPayments/verify_connect_account&${process.env.REACT_APP_STRIPE_CONNECT_CLIENT_ID}&state=${uuid()}`, '_blank');
                     win.focus()
                 }}>
                     Verify account
