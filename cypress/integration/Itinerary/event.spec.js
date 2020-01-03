e1b1e7687e1e983dea7990798625f599dfd5951f
Jordan Boudreau <jordboudreau@gmail.com>
2020-01-03 15:12:23 -0800

    fix hours bug on itinerary

--- cypress/integration/Itinerary/event.spec.js
+++ cypress/integration/Itinerary/event.spec.js
@@ -6,8 +6,10 @@ describe('Add, edit, and delete a event', () => {
     const e = {
         name: 'Fly to London',
         date: '01-10-2020',
-        startTime: '11:11 AM',
-        endTime: '02:00 PM',
+        start: '7',
+        end: '8',
+        startTime: '7:00 AM',
+        endTime: '8:00 AM',
         description:
             'You will meet at LAX and Karen will meet you there to hand out your passports. Once in London, meet up Mark near terminal 5',
         type: 'EVENT',
@@ -28,14 +30,23 @@ describe('Add, edit, and delete a event', () => {
         cy.get('input[id="event-name"]')
             .type(e.name)
         cy.get(`input[value="${e.type}"]`).click()
+        cy.get('input[name="start"]')
+            .clear().type(e.startTime)
+        cy.get('input[name="end"]')
+            .clear().type(e.endTime)
         cy.get('input[name="dateStart"]')
             .type(e.date)
         cy.get('input[name="description"]')
             .type(e.description)
         cy.get('input[id="google-map-places"]')
-            .type(e.location).type('{esc}')
-        cy.get('input[value="links"]')
-            .type(e.links).type(e.links)
+            .type(e.location).type('{downarrow}').type('{enter}')
+        cy.get('div[id="documents-multiple-select"]').click()
+        cy.get('li').contains(e.documents[0]).click()
+        cy.get('li').contains(e.documents[1]).click().type('{esc}')
+        cy.get('input[name="links"]')
+            .type(e.links)
+        cy.get('.event-submit-button')
+            .click()
     })
 
 })
