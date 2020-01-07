89e384502382e230122b5e61983b073a966d817b
Jordan Boudreau <jordboudreau@gmail.com>
2020-01-07 12:22:55 -0800

    change check for connect account

--- src/Account/TravelerPayments.js
+++ src/Account/TravelerPayments.js
@@ -8,7 +8,8 @@ import uuid from 'react-uuid'
 export default class TravelerPayments extends Component {
 
     state = {
-        canRequestPayments: false
+        hasConnectAccount: false,
+        hasCheckedForConnectAccount: false
     }
 
     constructor(props) {
@@ -23,11 +24,12 @@ export default class TravelerPayments extends Component {
     getStripeAccount = async () => {
         try {
             const account = await apiCall('GET', '/api/stripe/connect')
-            this.setState({ canRequestPayments: account.charges_enabled })
+            this.setState({ hasConnectAccount: true })
         } catch (err) {
             this.setState({ canRequestPayments: false })
         }
 
+        this.setState({ hasVerifiedConnectAccount: true })
     }
 
     goToPaymentDashboard = async () => {
@@ -61,18 +63,21 @@ export default class TravelerPayments extends Component {
     render() {
         return (
             <Card style={{ padding: 16 }}>
-                <Typography variant="subtitle2">{!this.state.canRequestPayments ?
-                    'Your organization\'s payment is not yet verified, please verify your account to request payments from travelers. After you complete the form it may take up to 15 minutes to verify your account.'
-                    : 'You have verified your account. You can now request payments from you travelers. Go to your Payment Dashboard by clicking the button below. You can modify your payment settings and view your payouts there.'}</Typography>
-                {!this.state.canRequestPayments && <Button size="large" type="submit" variant="contained" color="secondary" id="signup" style={{ width: '180px', height: '50px', color: 'white', marginTop: '25px' }} onClick={() => {
-                    var win = window.open(`https://connect.stripe.com/express/oauth/authorize?redirect_uri=${process.env.REACT_APP_BASE_URL}/account/travelerPayments&client_id=${process.env.REACT_APP_STRIPE_CONNECT_CLIENT_ID}&state=${uuid()}`, '_blank');
-                    win.focus()
-                }}>
-                    Verify account
+                {this.state.hasVerifiedConnectAccount &&
+                    <>
+                        <Typography variant="subtitle2">{!this.state.hasConnectAccount ?
+                            'Your organization\'s payment is not yet verified, please verify your account to request payments from travelers. After you complete the form it may take up to 15 minutes to verify your account.'
+                            : 'You have verified your account. You can now request payments from you travelers. Go to your Payment Dashboard by clicking the button below. You can modify your payment settings and view your payouts there.'}</Typography>
+                        {!this.state.hasConnectAccount && <Button size="large" type="submit" variant="contained" color="secondary" id="signup" style={{ width: '180px', height: '50px', color: 'white', marginTop: '25px' }} onClick={() => {
+                            var win = window.open(`https://connect.stripe.com/express/oauth/authorize?redirect_uri=${process.env.REACT_APP_BASE_URL}/account/travelerPayments&client_id=${process.env.REACT_APP_STRIPE_CONNECT_CLIENT_ID}&state=${uuid()}`, '_blank');
+                            win.focus()
+                        }}>
+                            Verify account
                 </Button>}
-                <Button size="large" type="submit" disabled={!this.state.canRequestPayments} variant="contained" color="primary" id="signup" style={{ width: '180px', height: '50px', float: 'right', marginTop: '25px' }} onClick={this.goToPaymentDashboard}>
-                    GO TO DASHBOARD
+                        <Button size="large" type="submit" disabled={!this.state.hasConnectAccount} variant="contained" color="primary" id="signup" style={{ width: '180px', height: '50px', float: 'right', marginTop: '25px' }} onClick={this.goToPaymentDashboard}>
+                            GO TO DASHBOARD
                 </Button>
+                    </>}
             </Card>
         )
     }
