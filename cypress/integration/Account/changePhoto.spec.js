282aa2b67d2375ee9b0f9e83132deb7e0345c8e2
Jordan Boudreau <jordboudreau@gmail.com>
2020-01-08 16:04:55 -0800

    add account image test

---
+++ cypress/integration/Account/changePhoto.spec.js
@@ -0,0 +1,21 @@
+import loginAfterSeed from '../Util/loginAfterSeed'
+
+describe('update coordinators photo', () => {
+    loginAfterSeed()
+    const c = {
+        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Knuckles_the_Echidna.png/220px-Knuckles_the_Echidna.png'
+    }
+
+    it('should update a coordinators photo', function () {
+
+        cy.contains('Admin Administrator').next().click()
+        cy.get('li').contains('Account').click()
+        cy.get('.img-box').click()
+
+        cy.get('input[name="link"]')
+            .type(c.image)
+        cy.get('button[type="submit"]').contains('Submit').click()
+        cy.get('.img-box').children().first().should('have.attr', 'src', c.image)
+
+    })
+})
