633b7b286c09b4071fbaf9832c1bde846cf6d2cf
Jordan Boudreau <jordboudreau@gmail.com>
2020-01-10 12:10:44 -0800

    add new navbar popup

--- src/util/otherComponents/Navbar.js
+++ src/util/otherComponents/Navbar.js
@@ -5,15 +5,21 @@ import { logout } from '../redux/actions/auth'
 import './Navbar.css'
 import Fab from '@material-ui/core/Fab'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import Menu from '@material-ui/core/Menu'
 import MenuItem from '@material-ui/core/MenuItem'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
+import MenuList from '@material-ui/core/MenuList'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import Grow from '@material-ui/core/Grow';
+import Paper from '@material-ui/core/Paper';
+import Popper from '@material-ui/core/Popper';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 
 class Navbar extends Component {
     state = {
-        anchorEl: null
+        open: false
     }
 
     constructor(props) {
@@ -40,11 +46,18 @@ class Navbar extends Component {
     }
 
     handleClick = (event) => {
-        this.setState({ anchorEl: event.currentTarget })
+        this.setState({ open: true })
     }
 
     handleClose = () => {
-        this.setState({ anchorEl: null })
+        this.setState({ open: false })
+    }
+
+    handleListKeyDown = event => {
+        if (event.key === 'Tab') {
+            event.preventDefault()
+            this.handleClose(false)
+        }
     }
 
     render() {
@@ -98,19 +111,7 @@ class Navbar extends Component {
                     <div className="navbar-nav">
                         <ul className="nav navbar-nav navbar-right d-flex d-row align-items-center">
                             <li className="Navbar user-name">{currentUser.name}</li>
-                            <IconButton style={{ color: 'white', backgroundColor: '#83C9F4', padding: 0, marginLeft: '16px' }} onClick={this.handleClick}>
-                                <ExpandMoreIcon />
-                            </IconButton>
-                            <Menu
-                                id="simple-menu"
-                                anchorEl={this.state.anchorEl}
-                                open={Boolean(this.state.anchorEl)}
-                                onClose={this.handleClose}
-                            >
-                                <MenuItem onClick={this.account}>Account</MenuItem>
-                                <MenuItem onClick={this.support}>Support</MenuItem>
-                                <MenuItem onClick={this.signout}>Logout</MenuItem>
-                            </Menu>
+                            <MenuListComposition account={this.account} support={this.support} signout={this.signout}></MenuListComposition>
                         </ul>
                     </div>
                 </div>
@@ -119,7 +120,7 @@ class Navbar extends Component {
         }
 
         return (
-            <nav className="Left-Navbar navbar navbar navbar-expand container-fluid bg-primary" style={{ zIndex: 2, paddingLeft: 32, paddingRight: 32 }} >
+            <nav className="Left-Navbar navbar navbar navbar-expand container-fluid bg-primary" style={{ zIndex: 2, paddingLeft: 64, paddingRight: 64 }} >
                 {!tripName ? <div className="navbar-brand">
                     <Link to={linkTo} class="navbar-brand ">
                         <img src="/left.png" alt="" style={{ height: '40px' }}></img>
@@ -161,9 +162,98 @@ const mapStateToProps = state => {
     }
 }
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    paper: {
+        marginRight: theme.spacing(2),
+    },
+}));
+
+function MenuListComposition({ account, signout, support }) {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen(prevOpen => !prevOpen);
+    };
+
+    const handleClose = event => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
+    return (
+        <div className={classes.root}>
+            <div>
+                <IconButton style={{ color: 'white', backgroundColor: '#83C9F4', padding: 0, marginLeft: '16px' }} onClick={handleToggle} ref={anchorRef}>
+                    <ExpandMoreIcon />
+                </IconButton>
+                <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal placement='bottom'>
+                    {({ TransitionProps, placement }) => (
+                        <Grow
+                            {...TransitionProps}
+                        >
+                            <Paper>
+                                <ClickAwayListener onClickAway={handleClose}>
+                                    <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                        <MenuItem onClick={account}>Account</MenuItem>
+                                        <MenuItem onClick={support}>Support</MenuItem>
+                                        <MenuItem onClick={signout}>Logout</MenuItem>
+                                    </MenuList>
+                                </ClickAwayListener>
+                            </Paper>
+                        </Grow>
+                    )}
+                </Popper>
+            </div>
+        </div>
+    );
+}
+
 export default withRouter(
     connect(
         mapStateToProps,
         { logout }
     )(Navbar)
 )
+
+    // < Popper open = { this.state.open } anchorEl = { anchorRef.current } role = { undefined } transition disablePortal >
+    //     {({ TransitionProps, placement }) => (
+    //         <Grow
+    //             {...TransitionProps}
+    //         >
+    //             <Paper>
+    //                 <ClickAwayListener onClickAway={handleClose}>
+    //                     <MenuList autoFocusItem={this.state.open} id="menu-list-grow" onKeyDown={this.handleListKeyDown}>
+    //                         <MenuItem onClick={this.account}>Account</MenuItem>
+    //                         <MenuItem onClick={this.support}>Support</MenuItem>
+    //                         <MenuItem onClick={this.signout}>Logout</MenuItem>
+    //                     </MenuList>
+    //                 </ClickAwayListener>
+    //             </Paper>
+    //         </Grow>
+    //     )}
+    //                         </Popper >
