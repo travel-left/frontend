2d9d6398740cbeb5bdc5ea70a4ef29aed27c8e0f
Jordan Boudreau <jordboudreau@gmail.com>
2020-02-10 16:33:41 -0800

    fix shadow bug on cover photo

--- src/TripDashboard/Cover/index.js
+++ src/TripDashboard/Cover/index.js
@@ -20,7 +20,7 @@ const styles = theme => ({
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end',
-        boxShadow: "0px 2px 1px - 1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
+        boxShadow: theme.shadows[4],
         backgroundImage: props => `url(${props.currentTrip.image})`,
         height: theme.spacing(24),
         backgroundPosition: 'center',
